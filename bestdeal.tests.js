2175cfdc9f916003f3e2e5aaeb3d959e39638122
vivo8934 <mfundo8934@gmail.com>
2017-05-10 15:51:46 +0200

    trying to calculate the cost of 1 avo

--- bestdeal.tests.js
+++ bestdeal.tests.js
@@ -3,7 +3,16 @@ describe('Conversion', function(){
 it('it should convert string to an array', function(){
 
 var allConversion = Conversion('1 for R3, 2 for R7, 3 for R10, 5 for R14.50.');
-  
+
 });
 
 });
+ describe('CostPerAvo', function(){
+
+   it('it should calculate the price per avo in each deal', function(){
+
+ var price = CostPerAvo('3 for R10');
+ assertDeep.equal(3.33, price);
+
+   });
+ });
