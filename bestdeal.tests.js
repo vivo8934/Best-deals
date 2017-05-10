1d5354d84292a285436728b86fa72ca54d50374d
vivo8934 <mfundo8934@gmail.com>
2017-05-10 15:16:20 +0200

    first test converting string to an array

---
+++ bestdeal.tests.js
@@ -0,0 +1,9 @@
+describe('Conversion', function(){
+
+it('it should convert string to an array', function(){
+
+var allConversion = Conversion('1 for R3, 2 for R7, 3 for R10, 5 for R14.50.');
+  
+});
+
+});
